f3d577b3686f84744f8181a2e730e384fa34139b
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-12-25 18:31:58 +0300

    initial commit

---
+++ src/main/java/ru/netology/unit/BonusService.java
@@ -0,0 +1,18 @@
+package ru.netology.unit;
+
+public class BonusService {
+    public int calcBonus(int amount) {
+
+
+        if (amount < 1000) {
+            return 0;
+        }
+        int bonus = (amount - 1000) / 100;
+        if (bonus > 100) {
+            return 100;
+        }
+        return bonus;
+    }
+}
+
+
